197136994d15ef1526b8bd79d89e861a5cd131a3
Reno Francis <grenofrancis@users.noreply.github.com>
2021-03-22 14:12:47 +0530

    Trying the same as before with string.contains method

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/MessagingExtensionQnaCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/MessagingExtensionQnaCard.cs
@@ -980,8 +980,8 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                         Url = new Uri(answerModel?.RedirectionUrl),
                     });
             }
-      string answer = JsonConvert.DeserializeObject<AnswerModel>(qnaDocument.Answer)?.Description;
-      if (answer.Contains("https://microsoftapc.sharepoint.com"))
+      //string answer = JsonConvert.DeserializeObject<AnswerModel>(qnaDocument.Answer)?.Description;
+      if (string.Contains(answerModel?.Description,"https://microsoftapc.sharepoint.com"))
       {
           responseCard.Actions.Add(
             new AdaptiveSubmitAction
